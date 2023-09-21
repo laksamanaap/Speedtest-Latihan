83e9b05c2cb5c8d2d9603b83907199c02f35f2c8
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-21 23:46:26 +0700

    feat : d2 - website watermark image

--- D2 - Website Watermark/index.php
+++ D2 - Website Watermark/index.php
@@ -3,12 +3,12 @@
 function addWatermark($imageURL, $outputPath) {
     $image = imagecreatefromjpeg($imageURL);
     if (!$image) {
-        print_r("unable to open image");
+        die("<h1>unable to open image!</h1>");
     }
 
     $watermark = imagecreatefrompng('logo.png');
     if (!$watermark) {
-        print_r("unable to open image");
+        die("<h1>unable to open image!</h1>");
     }    
 
     $image_width = imagesx($image);
@@ -19,12 +19,36 @@ function addWatermark($imageURL, $outputPath) {
 
     // Calculate the position for the watermark (e.g., top right corner)
     
-    $watermark_x = $image_width - $watermark_width;
-    $watermark_y = 0;
-    // print_r($watermark_x);
+    var_dump("image width : $image_width");
+    echo "<br>";
+    var_dump("watermark width : $watermark_width");
+    echo "<br>";
 
-    // Merge the watermark onto the image
-    imagecopymerge($image, $watermark, $watermark_x, $watermark_y, 0, 0, $watermark_width, $watermark_height, 50);
+    $watermark_x = $image_width / 2;
+    $watermark_y = 0;
+    var_dump("watermark x : $watermark_x");
+
+
+    // Merge the watermark into the image
+
+    /* 
+    bool imagecopymerge ( $dst_image, $src_image, $dst_x, $dst_y, 
+    $src_x, $src_y, $src_w, $src_h, $pct )
+
+    $dst_image: This parameter is used to set the destination image link resource.
+    $src_image: This parameter is used to set the source image link resource.
+    $dst_x: This parameter is used to set the x-coordinate of the destination point.
+    $dst_y: This parameter is used to set the y-coordinate of the destination point.
+    $src_x: This parameter is used to set the x-coordinate of the source point.
+    $src_y: This parameter is used to set the x-coordinate of the source point.
+    $src_w: This parameter is used to set source width.
+    $src_h: This parameter is used to set source height.
+    $pct: The two images will be merged with the help of $pct variables. The range of pct is 0 to 100. 
+    If $pct = 0, then no action is taken, and when $pct = 100 then this function behaves similarly to imagecopy()
+    function for palette images, except ignoring the alpha components. It implements alpha transparency for true color images.
+    */
+
+    imagecopymerge($image, $watermark, $watermark_x, $watermark_y, 0, 0, $watermark_width, $watermark_height, 60);
 
     // Save the watermarked image
     imagejpeg($image, $outputPath);
