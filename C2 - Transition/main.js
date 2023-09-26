3c28cb15a29bdb4b8cfc000eb358993127f52edd
laksamanaap <laksamana.arya1412@gmail.com>
2023-09-26 15:28:28 +0700

    fix : transitions when each card clicked

--- C2 - Transition/main.js
+++ C2 - Transition/main.js
@@ -14,10 +14,18 @@ document.getElementById("splitButton").addEventListener("click", function () {
     gridContainer.style.setProperty('--y', y);
 
     for (let i = 0; i < x * y; i++) {
-        const cell = document.createElement('div');
-        cell.className = 'grid-cell';
+        let cell = document.createElement('div');
+        cell.className = `grid-cell grid-cell--${i}`;
+        
         gridContainer.appendChild(cell);
+
+        cell.addEventListener("click", () => {
+            cell.classList.add("grid-cell-animated");
+        })
     }
+
+
+
 
 
 })
